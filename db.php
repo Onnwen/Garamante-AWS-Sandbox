125ea661443b30ab1cdb9004e00730a1ddddb25c
Onnwen <onnwen.cassitto@icloud.com>
2023-01-25 10:20:27 +0100

    Modifiche minori

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+
+$db = [
+    'db_engine' => 'mysql',
+    'db_host' => 'db.garamante.it',
+    'db_name' => 'Ecommerce',
+    'db_user' => 'admin',
+    'db_password' => 'lpgbFc2d#9N0',
+];
