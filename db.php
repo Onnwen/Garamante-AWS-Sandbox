382b4657d7bd9302a0c14e4bcd7047449b84a028
Onnwen <onnwen.cassitto@icloud.com>
2023-01-25 10:30:22 +0100

    Cancellati file

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db = [
-    'db_engine' => 'mysql',
-    'db_host' => 'db.garamante.it',
-    'db_name' => 'Ecommerce',
-    'db_user' => 'admin',
-    'db_password' => 'lpgbFc2d#9N0',
-];
